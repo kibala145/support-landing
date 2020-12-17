4559783e7945b3c096c77b5b04fef32fb7826256
kibala <kibalaprogramming@gmail.com>
2020-12-17 17:50:36 +0200

    chore: webpack config

--- src/components/AppDialog/AppDialog.tsx
+++ src/components/AppDialog/AppDialog.tsx
@@ -1,6 +1,6 @@
 import React, { MouseEventHandler } from 'react'
 import styles from './AppDialog.module.scss'
-import { ReactComponent as IconCross } from './../../images/icons/cross.svg'
+import { ReactComponent as IconCross } from '@/images/icons/cross.svg'
 
 const AppDialog: React.FC<Props> = ({ children, onClose }) => (
   <div className={styles.AppDialog}>
